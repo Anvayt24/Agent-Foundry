988b3e30c5d81e70de04ac64c894d648b4eca92e
anvay <anvatrive2324@gmail.com>
2025-08-10 16:22:09 +0530

    Initial commit of my project

---
+++ load_docs.py
@@ -0,0 +1,13 @@
+from langchain_community.document_loaders import Directoryloader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+def load_and_chunk_doc (path:str , chunk_size = 500, chunk_overlap = 50):
+    loader = Directoryloader(path, glob="**/*.txt")
+    docs = loader.load()
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size = chunk_size,
+        chunk_overlap = chunk_overlap
+    )
+    return splitter.split_documents(docs)
+     
+
---
+++ vector_store.py
@@ -0,0 +1,14 @@
+from langchain_community.vectorstores import Chroma
+from langchain.embeddings import SentenceTransformerEmbeddings
+from load_docs import load_and_chunk_docs
+
+def build_vector_store(data_path: str, persist_directory="rag_db"):
+    docs = load_and_chunk_docs(data_path)
+    embeddings = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+    vectordb = Chroma.from_documents(
+        documents=docs,
+        embedding=embeddings,
+        persist_directory=persist_directory
+    )
+    vectordb.persist()
+    return vectordb
